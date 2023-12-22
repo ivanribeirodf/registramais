8b46654ff204caed785accc1ad32b28fb95fd218
IvanPix <ivan@pix.com.br>
2023-12-22 11:23:37 -0300

    Classe de cadastro

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -6,27 +6,27 @@
 
     <div class="form-group">
       <label>First Name</label>
-      <input type="text" class="form-control" placeholder="First Name" />
+      <input type="text" class="form-control" v-model="first_name" placeholder="First Name" />
     </div>
 
     <div class="form-group">
       <label>Last Name</label>
-      <input type="text" class="form-control" placeholder="Last Name" />
+      <input type="text" class="form-control" v-model="last_name" placeholder="Last Name" />
     </div>
 
     <div class="form-group">
       <label>Email</label>
-      <input type="text" class="form-control" placeholder="Email" />
+      <input type="text" class="form-control" v-model="email" placeholder="Email" />
     </div>
 
     <div class="form-group">
       <label>Password</label>
-      <input type="text" class="form-control" placeholder="Password" />
+      <input type="password" class="form-control" v-model="password" placeholder="Password" />
     </div>
 
     <div class="form-group">
       <label>Confirm Password</label>
-      <input type="text" class="form-control" placeholder="Confirm Password" />
+      <input type="password" class="form-control" v-model="password_confirm" placeholder="Confirm Password" />
     </div>
 
     <button class="btn btn-primary btn-block">Sign Up</button>
@@ -36,11 +36,40 @@
 </template>
 
 <script>
+  import axios from 'axios'
   export default {
     name: 'Register',
+    data() {
+      return {
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: '',
+        password_confirm: ''
+
+      }
+    },
     methods: {
-      handleSubmit(){     
-        console.log('submitted')
+      handleSubmit(){
+        const data = {
+          first_name: this.first_name,
+          last_name: this.last_name,
+          email: this.email,
+          password: this.password,
+          password_confirm: this.password
+        };
+        
+        axios.post('https://localhost:8000/register', data)
+          .then(
+            res => {
+              console.log(res)
+            }
+          ).catch(
+            err => {
+              console.log(err)
+            }
+          )
+        
       }
     }
   }
