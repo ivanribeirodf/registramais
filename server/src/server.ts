de9f146cbec72140b489f10b8a80be31b349abf1
ivanribeirodf <ivandf@gmail.com>
2023-12-30 18:42:44 -0300

    Nova rota

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,25 @@
-import fastify from 'fastify'
-import fastifyCors from '@fastify/cors'
-import { userCreate } from './routes/user'
+import express, { Request, Response, NextFunction, Router} from 'express'
+import 'express-async-errors'
 
-const app = fastify()
-app.register(fastifyCors)
-app.register(userCreate)
+import { router } from './routes'
+import cors from 'cors';
 
-app
-  .listen({
-    port: 8000,
+const app = express();
+app.use(express.json())
+app.use(cors())
+
+app.use(router)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if(err instanceof Error){
+    return res.status(400).json({
+      error: err.message
+    })
+  }
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error.'
   })
-  .then(() => {
-    console.log('🚀 HTTP server running on <http://localhost:8000>')
-  })
+})
+
+app.listen(8000, () => console.log('🚀 HTTP server running on <http://localhost:8000>'))
